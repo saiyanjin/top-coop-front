b53701774d6bf6fee20ca39eac3f0c95a4b16bf1
saiyanjin <115701346+saiyanjin@users.noreply.github.com>
2026-06-08 11:36:29 +0200

    page connexion
    composant connexion, layout login, page connexion

---
+++ app/components/ConnexionForm.vue
@@ -0,0 +1,73 @@
+<template>
+  <h2 class="text-center text-orange">
+    Connexion au <br>
+    <span class="d-flex align-center justify-center ga-2"><v-icon icon="mdi-view-dashboard-outline"></v-icon>Dashboard Top'Coop</span>
+  </h2>
+
+  <v-form ref="formRef" class="mt-10" v-model="isFormValid" @submit.prevent="">
+    <v-text-field
+      v-model="email"
+      label="Adresse email"
+      type="email"
+      variant="outlined"
+      density="comfortable"
+      color="vertFonce"
+      prepend-inner-icon="mdi-email-outline"
+      :rules="emailRules"
+      required
+      rounded="lg"
+      class="mb-3"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password"
+      label="Mot de passe"
+      :type="showPassword ? 'text' : 'password'"
+      variant="outlined"
+      density="comfortable"
+      color="vertFonce"
+      prepend-inner-icon="mdi-lock-outline"
+      :append-inner-icon="showPassword ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
+      @click:append-inner="showPassword = !showPassword"
+      :rules="passwordRules"
+      required
+      rounded="lg"
+      class="mb-6"
+      ></v-text-field>
+
+    <v-btn
+      type="submit"
+      color="orange"
+      size="large"
+      block
+      rounded="lg"
+      :loading="isSubmitting"
+      :disabled="!isFormValid"
+      class="text-text font-weight-bold"
+      elevation="1"
+    >
+    Connexion
+    </v-btn>
+  </v-form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isFormValid = ref(false)
+const isSubmitting = ref(false)
+const showPassword = ref(false)
+const email = ref('')
+const password = ref('')
+
+// ---------- les règles ----------
+const emailRules = [
+  (v: string) => !!v || "L'adresse email est requise.",
+  (v: string) => /.+@.+\..+/.test(v) || "L'adresse email doit être valide."
+]
+
+const passwordRules = [
+  (v: string) => !!v || 'Le mot de passe est requis.',
+]
+
+</script>
---
+++ app/pages/connexion.vue
@@ -0,0 +1,17 @@
+<template class=" d-flex justify-center">
+  <v-container 
+    class="rounded-xl elevation-1 bg-white pa-15"
+    max-width="600"
+    min-width="400"
+  >
+    <connexion-form />
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import ConnexionForm from '~/components/ConnexionForm.vue';
+
+definePageMeta({
+  layout: 'login'
+})
+</script>
