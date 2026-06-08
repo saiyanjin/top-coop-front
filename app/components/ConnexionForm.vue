4aa4f536438acd375a7c46c7db4f5df217675f6d
saiyanjin <115701346+saiyanjin@users.noreply.github.com>
2026-06-08 11:42:24 +0200

    maj

--- app/components/ConnexionForm.vue
+++ app/components/ConnexionForm.vue
@@ -1,54 +1,60 @@
 <template>
-  <h2 class="text-center text-orange">
-    Connexion au <br>
-    <span class="d-flex align-center justify-center ga-2"><v-icon icon="mdi-view-dashboard-outline"></v-icon>Dashboard Top'Coop</span>
-  </h2>
+  <div style="max-width: 420px;" class="mx-auto w-100">
+    
+    <h2 class="text-center text-orange">
+      Connexion au <br>
+      <span class="d-flex align-center justify-center ga-2">
+        <v-icon icon="mdi-view-dashboard-outline"></v-icon>Dashboard Top'Coop
+      </span>
+    </h2>
 
-  <v-form ref="formRef" class="mt-10" v-model="isFormValid" @submit.prevent="">
-    <v-text-field
-      v-model="email"
-      label="Adresse email"
-      type="email"
-      variant="outlined"
-      density="comfortable"
-      color="vertFonce"
-      prepend-inner-icon="mdi-email-outline"
-      :rules="emailRules"
-      required
-      rounded="lg"
-      class="mb-3"
-    ></v-text-field>
-
-    <v-text-field
-      v-model="password"
-      label="Mot de passe"
-      :type="showPassword ? 'text' : 'password'"
-      variant="outlined"
-      density="comfortable"
-      color="vertFonce"
-      prepend-inner-icon="mdi-lock-outline"
-      :append-inner-icon="showPassword ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
-      @click:append-inner="showPassword = !showPassword"
-      :rules="passwordRules"
-      required
-      rounded="lg"
-      class="mb-6"
+    <v-form ref="formRef" class="mt-10" v-model="isFormValid" @submit.prevent="">
+      <v-text-field
+        v-model="email"
+        label="Adresse email"
+        type="email"
+        variant="outlined"
+        density="comfortable"
+        color="vertFonce"
+        prepend-inner-icon="mdi-email-outline"
+        :rules="emailRules"
+        required
+        rounded="lg"
+        class="mb-3"
       ></v-text-field>
 
-    <v-btn
-      type="submit"
-      color="orange"
-      size="large"
-      block
-      rounded="lg"
-      :loading="isSubmitting"
-      :disabled="!isFormValid"
-      class="text-text font-weight-bold"
-      elevation="1"
-    >
-    Connexion
-    </v-btn>
-  </v-form>
+      <v-text-field
+        v-model="password"
+        label="Mot de passe"
+        :type="showPassword ? 'text' : 'password'"
+        variant="outlined"
+        density="comfortable"
+        color="vertFonce"
+        prepend-inner-icon="mdi-lock-outline"
+        :append-inner-icon="showPassword ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
+        @click:append-inner="showPassword = !showPassword"
+        :rules="passwordRules"
+        required
+        rounded="lg"
+        class="mb-6"
+      ></v-text-field>
+
+      <v-btn
+        type="submit"
+        color="orange"
+        size="large"
+        block
+        rounded="lg"
+        :loading="isSubmitting"
+        :disabled="!isFormValid"
+        class="text-text font-weight-bold"
+        elevation="1"
+      >
+        Connexion
+      </v-btn>
+    </v-form>
+
+  </div>
 </template>
 
 <script setup lang="ts">
--- app/pages/connexion.vue
+++ app/pages/connexion.vue
@@ -1,11 +1,16 @@
-<template class=" d-flex justify-center">
-  <v-container 
-    class="rounded-xl elevation-1 bg-white pa-15"
-    max-width="600"
-    min-width="400"
-  >
-    <connexion-form />
-  </v-container>
+<template>
+  <v-row class="fill-height ma-0 d-flex align-center justify-center">
+    <v-col cols="12" sm="10" md="8" lg="6" class="d-flex justify-center">
+      
+      <v-container 
+        class="rounded-xl elevation-1 bg-white pa-8 pa-sm-15"
+        max-width="600"
+      >
+        <connexion-form />
+      </v-container>
+
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
